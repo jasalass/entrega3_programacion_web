5356e517be8af5e811066b5f40b0cf9b01726957
Vina del Mar <vina@duoc.acad>
2024-06-26 20:11:45 -0400

    Entrega final

--- caos_news/subirNoticia/views.py
+++ caos_news/subirNoticia/views.py
@@ -5,7 +5,9 @@ from .models import Noticia
 
 @login_required
 def subirNoticia(request):
+    
     if request.method == 'POST':
+    
         form = NoticiaForm(request.POST, request.FILES)
         if form.is_valid():
             noticia = form.save(commit=False)
@@ -13,13 +15,20 @@ def subirNoticia(request):
             noticia.save()
             return redirect('listar-noticias')  # Redirigir a la página principal después de guardar la noticia
     else:
-        form = NoticiaForm()
-        return render(request, 'subir-noticia.html', {'form': form})
+        if not request.user.periodista:
+            return redirect('index')
+        else:
+            form = NoticiaForm()
+            return render(request, 'subir-noticia.html', {'form': form})
 
 @login_required
 def lista_noticias(request):
-    noticias = Noticia.objects.filter(usuario=request.user)
-    return render(request, 'listar-noticias.html', {'noticias': noticias})
+    if not request.user.periodista:
+            return redirect('index')
+    else:
+        noticias = Noticia.objects.filter(usuario=request.user)
+        return render(request, 'listar-noticias.html', {'noticias': noticias})
+
 
 @login_required
 def actualizar_noticia(request, pk):
@@ -30,8 +39,12 @@ def actualizar_noticia(request, pk):
             form.save()
             return redirect('listar-noticias')
     else:
-        form = NoticiaForm(instance=noticia)
-    return render(request, 'actualizar_noticia.html', {'form': form})
+        if not request.user.periodista:
+            return redirect('index')
+        else:
+            form = NoticiaForm(instance=noticia)
+            return render(request, 'actualizar_noticia.html', {'form': form})
+        
 
 @login_required
 def eliminar_noticia(request, pk):
